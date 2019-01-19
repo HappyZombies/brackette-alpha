ff454291295b8e9b12ac9e2422971d33f7ecb36c
Daniel Reguero <dreguero@techrev.com>
2019-01-19 10:35:22 -0500

    some UI updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button variant="contained" color="primary">
+          Hello There
+        </Button>
+        <Icon>star</Icon>
       </div>
     );
   }
