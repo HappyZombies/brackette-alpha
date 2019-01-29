af5f5b2f61411f74738b28712dcb7f20f57849e8
HappyZombies <daniel.reguero@hotmail.com>
2019-01-28 20:20:56 -0500

    added icons on navbar

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { AppBar, Button, Icon, Toolbar, Typography } from "@material-ui/core";
-import { ButtonProps } from "@material-ui/core/Button";
+import { AppBar, Button, Toolbar, Typography, IconButton } from "@material-ui/core";
+import Icon from '@mdi/react'
+import { mdiGithubFace, mdiTwitter, mdiReddit, mdiDiscord } from '@mdi/js'
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -9,15 +10,45 @@ const Nav = () => {
   return (
     <div className="app-bar-wrapper">
       <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h5"
-            color="inherit"
-            className="grow"
-            component={({ innerRef, ...props }) => <Link {...props} to="/" />}
-          >
-            Brackette
+        <Toolbar style={{ justifyContent: "space-between" }}>
+          <span className="grow">
+            <Typography
+              variant="h5"
+              color="inherit"
+              id="logo"
+              component={({ innerRef, ...props }) => <Link {...props} to="/" />}
+            >
+              Brackette
           </Typography>
+            <IconButton href="https://github.com/happyzombies/brackette-alpha" target="_blank" className="grow">
+              <Icon
+                path={mdiGithubFace}
+                size={1}
+                color="#24292D"
+              />
+            </IconButton>
+            <IconButton href="https://github.com/happyzombies/brackette-alpa" target="_blank" className="grow">
+              <Icon
+                path={mdiTwitter}
+                size={1}
+                color="#24292D"
+              />
+            </IconButton>
+            <IconButton href="https://github.com/happyzombies/brackette-alpa" target="_blank" className="grow">
+              <Icon
+                path={mdiDiscord}
+                size={1}
+                color="#24292D"
+              />
+            </IconButton>
+            <IconButton href="https://reddit.com/r/brackette" target="_blank" className="grow">
+              <Icon
+                path={mdiReddit}
+                size={1}
+                color="#24292D"
+              />
+            </IconButton>
+          </span>
           <Button color="inherit" component={({ innerRef, ...props }) => <Link {...props} to="/login" />}>
             Login/Register
           </Button>
