f2ed4b0541f054acedfd6d1ff70905d5d1f55966
HappyZombies <daniel.reguero@hotmail.com>
2020-05-30 13:16:04 -0400

    methods for updating properties in user service

--- server/services/UsersService.js
+++ server/services/UsersService.js
@@ -3,6 +3,11 @@ const { transaction } = require("objection");
 
 const Users = require("../models/Users");
 const AlphaTokens = require("../models/AlphaTokens");
+const {
+  authenticatedResponse,
+  validPassword,
+  generatePasswordHash,
+} = require("../utils");
 const logger = require("./Logger");
 const ApiError = require("./ApiError");
 
@@ -18,7 +23,7 @@ class UsersService {
       user = await Users.query().findById(id);
     } catch (e) {
       logger.warn(`Error querying users ${e.message}`);
-      throw new ApiError("Internal Server Error Retrieving User.");
+      throw ApiError.internalServerErr();
     }
     if (!user) throw new ApiError("Not Found", HttpStatus.NOT_FOUND);
     return user;
@@ -40,6 +45,62 @@ class UsersService {
       await AlphaTokens.query().patch({ userId: newUser.id }).where({ token });
       return newUser;
     });
+  }
+
+  async updateUser(currentUser, updatedUserObj) {
+    if (updatedUserObj.username) {
+      // trying to update username, check if it's available
+      const usernameIsTaken = await this.isUsernameAvailable(
+        updatedUserObj.username
+      );
+      if (usernameIsTaken) {
+        throw new ApiError("Username is taken.", 409);
+      }
+    }
+    if (updatedUserObj.email) {
+      // trying to update username, check if it's available
+      const emailIsTaken = await this.isEmailAvailable(updatedUserObj.email);
+      if (emailIsTaken) {
+        throw new ApiError("E-mail is taken.", 409);
+      }
+    }
+    let user;
+    try {
+      const { id } = currentUser;
+      user = await Users.query().patchAndFetchById(id, { ...updatedUserObj });
+    } catch (e) {
+      logger.error("Couldn't update user preferences. =>" + e);
+      throw ApiError.internalServerErr();
+    }
+    return authenticatedResponse(user);
+  }
+
+  async updateUsersPassword(currentUser, updatePasswordObj) {
+    const { newPassword, newPasswordConfirm, password } = updatePasswordObj;
+    if (newPassword !== newPasswordConfirm) {
+      throw new ApiError("Password must match.", 400);
+    }
+    let user;
+    try {
+      user = await this.getUserById(currentUser.id);
+    } catch (e) {
+      logger.error("Could not get user when updating password:" + e);
+      throw ApiError.internalServerErr();
+    }
+    if (!validPassword(password, user.password)) {
+      logger.error("Invalid Password given when updating user");
+      throw new ApiError("Invalid Password", 400);
+    }
+    const newPasswordHashed = generatePasswordHash(newPassword);
+    try {
+      user = await Users.query().patchAndFetchById(user.id, {
+        password: newPasswordHashed,
+      });
+    } catch (e) {
+      logger.error("Could not get user when updating password:" + e);
+      throw ApiError.internalServerErr();
+    }
+    return authenticatedResponse(user);
   }
 
   async isUsernameAvailable(username) {
