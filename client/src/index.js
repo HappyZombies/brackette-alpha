c3bad5e867afddb81f075fe3208af73573b4c354
HappyZombies <daniel.reguero@hotmail.com>
2020-06-04 08:47:30 -0400

    Added basic things for needed for material UI and redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import store from "./store";
+import { theme } from "./Theme";
+import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
