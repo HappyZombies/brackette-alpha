6c4a6effafd43ed00eeb8d1a60819cec03b39966
HappyZombies <daniel.reguero@hotmail.com>
2020-06-03 21:12:47 -0400

    removed comments, added material ui to templated page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
@@ -10,14 +11,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Button
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          variant="contained"
+          color="primary"
         >
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
